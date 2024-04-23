db1811a8f511df7e9c0156d7a89ba4aa22713e35
Levi Wiederhold <wiederli@mail.uc.edu>
2024-04-23 12:03:02 -0400

    Add project files.

---
+++ Final/Models/DbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Final.Models
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+        public DbSet<TeamMember> TeamMembers { get; set; }
+        // Add other DbSets here
+    }
+}
---
+++ Final/Models/TeamMember.cs
@@ -0,0 +1,11 @@
+﻿namespace Final.Models
+{
+    public class TeamMember
+    {
+        public int Id { get; set; }
+        public string FullName { get; set; }
+        public DateTime BirthDate { get; set; }
+        public string CollegeProgram { get; set; }
+        public string YearInProgram { get; set; }
+    }
+}
